195eaf9cc4d2fb69bf1190ef40f5202d056bb799
everAtWork <ever.at.work@gmail.com>
2021-12-13 13:41:58 +0200

    here's the big boy's logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import HomePage from './pages/homepage/homepage.component.jsx';
+import Header from './compo/header/header.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
+    <Header>
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route path='/shop' component={ShopPage}></Route>
       </Switch>
+    </Header>
     </div>
   );
 }
