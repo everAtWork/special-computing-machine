becf409f0330c3a103599800c9aca354ff76acb2
everAtWork <ever.at.work@gmail.com>
2021-12-13 19:04:59 +0200

    and the navbar  will be drawn

--- src/compo/header/header.component.jsx
+++ src/compo/header/header.component.jsx
@@ -1,15 +1,16 @@
 import { React, Component} from 'react'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/snowstar.svg'
+import  './header.component.styles.scss'
 
 export default class Header extends Component {
     render() {
         return (
-    <div className="header">
-        <Link className="logo-container">
-            <Logo className="logo"></Logo>
-        </Link>
-    </div>
+            <div className="header">
+                <Link className="logo-container">
+                    <Logo className="logo"></Logo>
+                </Link>
+            </div>
 
         )
     }
